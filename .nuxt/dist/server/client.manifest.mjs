29a9f88f3ce67d84d2ad3354b80640002142e2ec
andralaiskodat <andraqbz.com>
2024-07-04 15:02:16 +0700

    master

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,99 @@
+export default {
+  "../node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "De144rT3.js",
+    "name": "error-404",
+    "src": "../node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js",
+      "_DmbmAeAJ.js"
+    ],
+    "css": []
+  },
+  "error-404.CjTTbIxB.css": {
+    "file": "error-404.CjTTbIxB.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "../node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CHYeVYTC.js",
+    "name": "error-500",
+    "src": "../node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_DmbmAeAJ.js",
+      "../node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-500.B4KzowuE.css": {
+    "file": "error-500.B4KzowuE.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "../node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dc7jlg7a.js",
+    "name": "entry",
+    "src": "../node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "../node_modules/nuxt/dist/app/components/error-404.vue",
+      "../node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "_globalCSS": true
+  },
+  "_DmbmAeAJ.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DmbmAeAJ.js",
+    "name": "vue.f36acd1f",
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/profil.jpeg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "profil.BV3VX1GB.jpeg",
+    "src": "assets/profil.jpeg"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "3uHgmR9X.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "../node_modules/nuxt/dist/app/entry.js"
+    ],
+    "assets": [
+      "profil.BV3VX1GB.jpeg"
+    ]
+  },
+  "profil.BV3VX1GB.jpeg": {
+    "file": "profil.BV3VX1GB.jpeg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  }
+}
---
+++ .nuxt/dist/server/styles.mjs
@@ -0,0 +1,8 @@
+const interopDefault = r => r.default || r || []
+export default {
+  "../node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.KmCRwcVY.mjs').then(interopDefault),
+  "../node_modules/nuxt/dist/app/components/error-404.vue": () => import('./_nuxt/error-404-styles.CEmoXRt-.mjs').then(interopDefault),
+  "../node_modules/nuxt/dist/app/components/error-500.vue": () => import('./_nuxt/error-500-styles.Dq0oRtnV.mjs').then(interopDefault),
+  "../node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=c6c1efc1&lang.css": () => import('./_nuxt/error-404-styles.CqwhS73W.mjs').then(interopDefault),
+  "../node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=2c66185b&lang.css": () => import('./_nuxt/error-500-styles.Bj0roX-b.mjs').then(interopDefault)
+}
